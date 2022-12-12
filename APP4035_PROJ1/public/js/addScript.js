eaff2cb662655db338eaad02c48337913e939c05
UZzoma <57295185+UZzoma@users.noreply.github.com>
2022-12-13 02:43:40 +0300

    Commit to Main from Local Dev

---
+++ APP4035_PROJ1/public/js/addScript.js
@@ -0,0 +1,131 @@
+const server = 'http://localhost:3000';
+
+//Adds courses to json file
+async function addCourse() {
+    console.log("Received Course")
+    let cId = document.getElementById('idCourse').value;
+    let cName = document.getElementById('nameCourse').value;
+    let cDesc = document.getElementById('passCourse').value;
+    const url = "/course";
+    const course = {
+        id: cId,
+        c_name: cName,
+        description: cDesc
+    };
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(course),
+    };
+    const response = await fetch(url, options);
+
+    //Display a message when the course has been successfully added
+    if(response.status == 400){
+        const outcome = await response.json();
+        alert (outcome.message)
+    }
+
+    //Display a message when the course can't be added
+    if(response.status == 200){
+        const outcome = await response.json();
+        alert (outcome.message)
+    }
+}
+
+async function addPerson(){
+    //Get the info of the user
+    let pEmail = document.getElementById('mailStud').value;
+    let pId = document.getElementById('studID').value;
+    let pName = document.getElementById('studName').value;
+    let pPass = document.getElementById('studPass').value;
+   
+    const url = "/person";
+
+    let studying = [];
+    let teaching = [];
+    let boxes = document.querySelectorAll('.myChk');
+
+    // Cycle through all the checkboxes
+    boxes.forEach(box => {
+        //Verify is a checkbox is checked
+        if(box.checked){
+
+            //If it is checked get the two radios
+            let radio = document.getElementsByName(box.value)
+
+            //Cycle through both radios
+            radio.forEach(element => {
+
+                //Decide where to put the course depending on the value
+                if(element.checked){
+                    if(element.value == 'student'){
+                        studying.push(element.name)
+                    }
+                    else{
+                        teaching.push(element.name)
+                    }
+                }
+            });
+
+        }
+        
+    });
+
+    const person = {
+                email: pEmail,
+                id: pId,
+                stud_name: pName,
+                Password: pPass,
+                courses: {
+                    student:studying,
+                    lecturer:teaching
+                },
+                };
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(person),
+    };
+    const response = await fetch(url, options);
+
+    //Display a message when the user has been successfully added
+    if(response.status == 400){
+        const outcome = await response.json();
+        alert (outcome.message)
+    }
+
+    //Display a message when the user can't be added
+    if(response.status == 200){
+        const outcome = await response.json();
+        alert (outcome.message)
+    }
+}
+
+if(document.getElementById('person')){
+    document.getElementById('person').addEventListener('submit', (e)=>{
+    sID = parseInt(document.getElementById('studID').value);
+    if((isNaN(sID)) || (parseInt(sID)<600000) || (parseInt(sID)>=700000)){
+        alert('Invalid user ID')
+    }
+    else{
+        addPerson();
+    }
+    e.preventDefault
+})}
+
+if(document.getElementById('course')){
+document.getElementById('course').addEventListener('submit', (e)=>{
+    let cID = document.getElementById('idCourse').value;
+    if(cID.length!=7){
+        alert("Invalid Course ID")
+    }
+    else{
+    addCourse();}
+    e.preventDefault
+})}
---
+++ APP4035_PROJ1/server.js
@@ -0,0 +1,181 @@
+const express = require('express');
+const sessions = require("express-session");
+const JsonStore = require('express-session-json')(sessions)
+const path = require('path');
+const bodyparser = require("body-parser");
+const cookieParser = require('cookie-parser');
+const fileName = "People.json"
+const courseFile = "Courses.json"
+const fs = require('fs')
+const jsonParser = bodyparser.json();
+const oneDay = 1000 * 60 * 60 * 24;
+
+const credential = {
+    email: "admin@gmail.com",
+    password: "admin123"
+}
+
+// Load People json from file
+let rawData = fs.readFileSync(fileName);
+let data = JSON.parse(rawData);
+
+// Load Course json from file
+let rawCourse = fs.readFileSync(courseFile)
+let dataCourse = JSON.parse(rawCourse)
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ entended: true }))
+
+app.set('view engine', 'hbs');
+app.set('trust proxy',1)
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
+app.use(cookieParser())
+
+// Enables the use of Express session
+app.use(sessions({
+    secret: "thisadhjkasjdkashdjkasfasjkfasjkgfasdd4as65418947561984",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: oneDay },
+    store: new JsonStore()
+}))
+
+var session;
+var message;
+
+//home route 
+app.get('/', (request, response) => {
+    response.render('base', { title: "Login System", error:message });
+})
+
+// Admin Dashboard
+app.get('/dashboard', (request, response) => {
+    response.render('dashboard');
+})
+
+//Main page of stduent/lecturer login
+app.get('/dashboard1', (request, response) => {
+    response.render('student',{ name: session.user.stud_name})
+})
+
+//Get the session information
+app.get('/getSession',(request,response)=>{
+    response.send(session.user)
+})
+
+//Gets the page where the admin sees the courses
+app.get('/a_courses', (request, response) => {
+    if(session.user){
+    response.render('admin_courses.hbs');}
+})
+
+//Login Button
+app.post('/login', (request, response) => {
+    if (request.body.email == credential.email && request.body.password == credential.password) {
+        session = request.session;
+        session.user = request.body;
+        response.redirect('/dashboard') // If admin is logged
+    } else {
+        for (let i = 0; i < data.length; i++) {
+            if ((request.body.email == data[i].email) && (request.body.password == data[i].Password)) {
+                session = request.session;
+                session.user = data[i];
+                return response.redirect('/dashboard1')
+            }
+        }
+        message = "Incorrect username or password"
+        return response.redirect('/')
+    }
+})
+
+//Gets all the Student and lecturer and sends it to the front-end
+app.get('/getPeople', function(request, response) {
+    const js = fs.readFileSync(fileName, { root: __dirname });
+    response.send(js);
+})
+
+//Gets all the Courses and  sends to the front end
+app.get('/sendCourse', function(request, response) {
+    const js = fs.readFileSync(courseFile, { root: __dirname });
+    response.send(js);
+})
+
+//Terminates the sessions
+app.get('/logout', (request, response) =>{
+    message = ""
+    request.session.destroy((err) => {
+        if(err) throw err;
+        console.log("User logged out")
+        response.redirect('/'); // redirect to the home page
+    })
+});
+
+
+//Adds the user to the Person.json
+app.post("/person", jsonParser, (request, response) => {
+    try{
+        data.forEach(element => {
+        // the data isscreened before been added to the json file
+            if((element.id == request.body.id )|| (element.email == request.body.email)){
+                return response.json({
+                    statusCode: 400,
+                    method: request.method,
+                    message: "This user already exists"
+                });
+            }
+            
+        });
+        
+        // If the data does not exist it is been added into the json file
+        console.log("Received User")
+        data.push(request.body);
+        fs.writeFileSync(fileName, JSON.stringify(data, null, 2));
+        return response.json({
+            statusCode: 200,
+            method: request.method,
+            message: "User Inserted Successfully"
+        });
+    }
+    catch(err){
+        console.log("There was an error while inserting the user")
+    }
+});
+
+//Adds the course to the Courses.json
+app.post("/course", jsonParser, (request, response) => {
+    try{
+        //dataCourse is the Courses file
+        dataCourse.forEach(element => {
+        // the data is screened before been added to the json file
+            if((element.id == request.body.id) || (element.c_name == request.body.c_name)){
+                console.log("This already exists")
+                return response.json({
+                    statusCode: 400,
+                    method: request.method,
+                    message: "This course already exists"
+                });
+            }
+        });
+
+        // If the data does not exist it is been added into the json file
+        console.log("Received Course")
+        dataCourse.push(request.body);
+        fs.writeFileSync(courseFile, JSON.stringify(dataCourse, null, 2));
+        return response.json({
+            statusCode: 200,
+            method: request.method,
+            message: "Course Inserted Successfully"
+        });
+    }
+    catch(err){
+        console.log("There was an error while inserting the course")}
+});
+
+
+//Opens the port
+app.listen(port, () => { console.log("Listening to server 3000") });
